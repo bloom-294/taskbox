11668fff751d450ed0abcbe47db2f3e173857ec7
bloom-294 <non44921@gmail.com>
2024-07-22 14:42:41 +0900

    Build a simple component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,26 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)", "../src/components/**/*.stories.@(js|jsx)"],
   staticDirs: ["../public"],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
+    '@storybook/addon-a11y',
     "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm",
+    "@chromatic-com/storybook"
   ],
+
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
 };
 export default config;
