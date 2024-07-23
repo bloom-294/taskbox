bc1cb32a08d227fae5c3eb7f580aa803fcc249b0
bloom-294 <non44921@gmail.com>
2024-07-23 13:06:52 +0900

    Addons

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -30,7 +30,7 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
           name="title"
           id={`title-${id}`}
           placeholder="Input title"
-          style={{ backgroundColor: 'red' }}
+          style={{ textOverflow: 'ellipsis' }}
         />
       </label>
       {state !== "TASK_ARCHIVED" && (
